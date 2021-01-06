9a5b417ce165a6418a29b7ff948f575005aab09f
DawudKim <dwkim@joara.com>
2021-01-07 00:59:55 +0900

    21.01.06 리스트 뷰 ing

--- app/src/main/java/Bigbigdw/JoaraDW/Main/MainBanner_ListAdapter.java
+++ app/src/main/java/Bigbigdw/JoaraDW/Main/MainBanner_ListAdapter.java
@@ -4,28 +4,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
-
 import Bigbigdw.JoaraDW.R;
 
-public class MainBanner_ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class MainBanner_ListAdapter  extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
     private ArrayList<MainBanner_ListData> listData = new ArrayList<>();
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mainbanners, parent, false);
-        return new MainBanner_ListViewHolder(view);
+        return new MainBanner_ListViewholder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ((MainBanner_ListViewHolder)holder).onBind(listData.get(position));
+        ((MainBanner_ListViewholder)holder).onBind(listData.get(position));
     }
 
     @Override
@@ -36,18 +33,21 @@ public class MainBanner_ListAdapter extends RecyclerView.Adapter<RecyclerView.Vi
     public void addItem(MainBanner_ListData data) {
         listData.add(data);
     }
+
+    static class MainBanner_ListViewholder extends  RecyclerView.ViewHolder {
+
+        ImageView Image;
+
+        public MainBanner_ListViewholder(@NonNull View itemView) {
+            super(itemView);
+
+            Image = itemView.findViewById(R.id.Img);
+        }
+
+        public void onBind(MainBanner_ListData data){
+            Image.setImageResource(data.getImg());
+        }
+    }
 }
 
-class MainBanner_ListViewHolder extends  RecyclerView.ViewHolder {
 
-    ImageView Image;
-
-    public MainBanner_ListViewHolder(@NonNull View itemView) {
-        super(itemView);
-        Image = itemView.findViewById(R.id.Img);
-    }
-
-    public void onBind(MainBanner_ListData data){
-        Image.setImageResource(data.getImg());
-    }
-}
