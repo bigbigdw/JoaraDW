2889e76f2674ec1c69925ffb4825d70badc44c9e
DawudKim <dwkim@joara.com>
2021-01-13 03:04:46 +0900

    21.01.13 메인 페이지 드로워 및 바텀 내비 ing

--- app/src/main/java/Bigbigdw/JoaraDW/Main/Main.java
+++ app/src/main/java/Bigbigdw/JoaraDW/Main/Main.java
@@ -1,15 +1,55 @@
 package Bigbigdw.JoaraDW.Main;
 
-import android.app.Activity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import Bigbigdw.JoaraDW.R;
 
 
-public class Main extends Activity {
+public class Main extends AppCompatActivity {
+    private AppBarConfiguration AppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_vie);
+
+        AppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.fragment_main
+        ).setDrawerLayout(drawer).build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, AppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        BottomNavigationView navView = findViewById(R.id.nav_bottom);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.fragment_main)
+                .build();
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, AppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/Bigbigdw/JoaraDW/Test/Test.java
+++ app/src/main/java/Bigbigdw/JoaraDW/Test/Test.java
@@ -3,31 +3,32 @@ package Bigbigdw.JoaraDW.Test;
 import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 import Bigbigdw.JoaraDW.R;
 
-public class Test extends Activity {
+public class Test extends AppCompatActivity {
     private Test_ListAdapter adapter = new Test_ListAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
-        init();
-
-        //아이템 로드
-        adapter.setItems(new TestData().getData());
+        setContentView(R.layout.fragment_main);
+//        init();
+//
+//        //아이템 로드
+//        adapter.setItems(new TestData().getData());
 
     }
 
-    private void init() {
-        RecyclerView recyclerView = findViewById(R.id.MainBannerList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-    }
+//    private void init() {
+//        RecyclerView recyclerView = findViewById(R.id.MainBannerList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setAdapter(adapter);
+//    }
 
 
 }
