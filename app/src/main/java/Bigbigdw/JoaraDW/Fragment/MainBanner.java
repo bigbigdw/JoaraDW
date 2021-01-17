26ee54a1afcae15bceefe553ef56fe7a26087b34
DawudKim <dwkim@joara.com>
2021-01-18 00:31:23 +0900

    21.01.18 메인 배너 JSON 파싱

--- app/src/main/java/Bigbigdw/JoaraDW/Fragment/MainBanner.java
+++ app/src/main/java/Bigbigdw/JoaraDW/Fragment/MainBanner.java
@@ -1,21 +1,11 @@
 package Bigbigdw.JoaraDW.Fragment;
 
 import android.content.res.AssetManager;
-import android.graphics.Movie;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.synnapps.carouselview.CarouselView;
-import com.synnapps.carouselview.ImageListener;
 import com.synnapps.carouselview.ViewListener;
 
 import org.json.JSONArray;
@@ -26,38 +16,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
-import Bigbigdw.JoaraDW.R;
+interface SetMainBanner {
 
-public class Fragment_Main extends Fragment implements SetMainBanner{
-
-    CarouselView MainBanner;
-
-//    String[] MainBannerUrl = new String[0];
-    String[] MainBannerUrl = new String[8];
-
-    List<String> testList = new ArrayList<>(Arrays.asList(MainBannerUrl));
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
-
-        AssetManager assetManager = getActivity().getAssets();
-        MainBanner = root.findViewById(R.id.Carousel_MainBanner);
-
-//        MainBanner(assetManager, MainBanner);
-
-        SetMainBanner.MainBanner(assetManager, MainBannerUrl, MainBanner, viewListener);
-
-        return root;
-    }
-
-    private void MainBanner(AssetManager assetManager, CarouselView MainBanner)
+    static void MainBanner(AssetManager assetManager, String[] MainBannerUrl,CarouselView MainBanner, ViewListener viewListener)
     {
         try {
             InputStream is = assetManager.open("Main-Banner.Json");
@@ -97,15 +59,5 @@ public class Fragment_Main extends Fragment implements SetMainBanner{
         }
     }
 
-    ViewListener viewListener = position -> {
 
-        View customView = getLayoutInflater().inflate(R.layout.main_banner_top, null);
-
-        ImageView fruitImageView = customView.findViewById(R.id.fruitImageView);
-
-        Glide.with(requireActivity().getApplicationContext()).load(MainBannerUrl[position])
-                .into(fruitImageView);
-
-        return customView;
-    };
 }
