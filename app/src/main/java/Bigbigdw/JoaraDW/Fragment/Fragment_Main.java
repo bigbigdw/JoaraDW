90139dfe07a3901fa74e83b1daeae37ea706d030
DawudKim <dwkim@joara.com>
2021-01-23 22:52:34 +0900

    21.01.23 캐러셀 크기별 다르게 표현

--- app/src/main/java/Bigbigdw/JoaraDW/Fragment/Fragment_Main.java
+++ app/src/main/java/Bigbigdw/JoaraDW/Fragment/Fragment_Main.java
@@ -19,14 +19,9 @@ import androidx.fragment.app.Fragment;
 import com.bumptech.glide.Glide;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
-
 import java.util.ArrayList;
-
 import java.util.List;
-
 import Bigbigdw.JoaraDW.R;
-
-import static java.lang.Integer.parseInt;
 
 public class Fragment_Main extends Fragment implements Main_Banner{
 
@@ -43,43 +38,21 @@ public class Fragment_Main extends Fragment implements Main_Banner{
 
        Main_Banner.SetMainBanner(assetManager, MainBanner, imageListener, MainBannerURLs);
 
-//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
-//                1103 );
-//        MainBanner.setLayoutParams(layoutParams);
-
         return root;
     }
 
 
-//    ViewListener viewListener = position -> {
-//        View customView = getLayoutInflater().inflate(R.layout.main_banner_top, null);
-//
-//        ImageView CarouselImg = customView.findViewById(R.id.CarouselImage);
-//
-//        Glide.with(requireActivity().getApplicationContext()).load(MainBannerURLs.get(position))
-//                .into(CarouselImg);
-//        return customView;
-//    };
-
-
-
     ImageListener imageListener = (position, imageView) -> {
         imageView.setAdjustViewBounds(true);
-//        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
 
         ViewTreeObserver vto = imageView.getViewTreeObserver();
         vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
             public boolean onPreDraw() {
                 imageView.getViewTreeObserver().removeOnPreDrawListener(this);
-                Log.d("", "Height: " + imageView.getMeasuredHeight() + " Width: " + imageView.getMeasuredWidth());
 
                 double doubled = (imageView.getMeasuredWidth()/ 1.704) + 5;
-                float density = getContext().getResources().getDisplayMetrics().density;
-
-                Log.d("",  "density: " + density);
 
                 int finalHeight = Integer.parseInt(String.valueOf(Math.round(doubled)));
-                Log.d("", "finalHeight: " + finalHeight);
 
                 LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
                         finalHeight );
@@ -89,10 +62,8 @@ public class Fragment_Main extends Fragment implements Main_Banner{
             }
         });
 
-
             Glide.with(requireActivity().getApplicationContext()).load(MainBannerURLs.get(position))
             .into(imageView);
-
 
     };
 }
