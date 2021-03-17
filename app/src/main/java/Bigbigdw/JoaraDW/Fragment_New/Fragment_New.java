1f0b3c5deb2bc82a5849b37622b26159d234ba5f
DawudKim <dwkim@joara.com>
2021-03-17 19:38:47 +0900

    21.03.17 메인 최신작품 ING

--- app/src/main/java/Bigbigdw/JoaraDW/Fragment_New/Fragment_New.java
+++ app/src/main/java/Bigbigdw/JoaraDW/Fragment_New/Fragment_New.java
@@ -7,32 +7,84 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import Bigbigdw.JoaraDW.Main.Main_BookData_A;
 import Bigbigdw.JoaraDW.R;
 
 public class Fragment_New extends Fragment {
 
-    private final Main_BookListAdapter_New NewBookListAdapter = new Main_BookListAdapter_New();
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_new, container, false);
-        AssetManager assetManager = getActivity().getAssets();
-        NewBookList(root, assetManager, "Main_Tab_Best.json");
+
+        viewPager = root.findViewById(R.id.view_pager);
+        setupViewPager(viewPager);
+
+        tabLayout = root.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
         return root;
     }
 
-    public void NewBookList(View root, AssetManager assetManager, String BookType){
-        RecyclerView recyclerView = root.findViewById(R.id.Main_NewBookList);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL,false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(NewBookListAdapter);
-        NewBookListAdapter.setItems(new Main_BookData_A().getData(assetManager, BookType));
-        NewBookListAdapter.notifyDataSetChanged();
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new New_Tab_New(), "전체");
+        adapter.addFragment(new New_Tab_New(), "77FES");
+        adapter.addFragment(new New_Tab_New(), "기다무");
+        adapter.addFragment(new New_Tab_New(), "노블성실");
+        adapter.addFragment(new New_Tab_New(), "노블레스");
+        adapter.addFragment(new New_Tab_New(), "프리미엄");
+        adapter.addFragment(new New_Tab_New(), "무료");
+        adapter.addFragment(new New_Tab_New(), "완결");
+        adapter.addFragment(new New_Tab_New(), "단편");
+        viewPager.setAdapter(adapter);
     }
 
+    public static class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+    
 }
