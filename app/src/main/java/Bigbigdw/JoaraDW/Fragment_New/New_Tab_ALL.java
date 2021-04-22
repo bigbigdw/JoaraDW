eeb85e6e07d636aff57ef3550fb21488269c4a7e
DawudKim <dwkim@joara.com>
2021-04-22 21:15:38 +0900

    2021.04.22 선호작 등록 작업 ing 2

--- app/src/main/java/Bigbigdw/JoaraDW/Fragment_New/New_Tab_ALL.java
+++ app/src/main/java/Bigbigdw/JoaraDW/Fragment_New/New_Tab_ALL.java
@@ -20,12 +20,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import Bigbigdw.JoaraDW.Etc.HELPER;
 import Bigbigdw.JoaraDW.Main.Main_BookListData;
 import Bigbigdw.JoaraDW.R;
 
@@ -35,25 +33,13 @@ public class New_Tab_ALL extends Fragment {
     private ArrayList<Main_BookListData> items = new ArrayList<>();
     LinearLayout Wrap, Cover;
     String Store="";
-    String TOKEN;
+    String TOKEN = "";
     ImageView Favon;
     ImageView Favoff;
+    String ETC = "";
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_new_tab_all, container, false);
-
-        RequestQueue queue = Volley.newRequestQueue(getActivity());
-        String API = "/v1/book/list.joa";
-        String ETC = "&store=" + Store + "&orderby=redate&offset=25&page=" + 1 + "&class=";
-        recyclerView = root.findViewById(R.id.Main_NewBookList);
-        Wrap = root.findViewById(R.id.Tab_NewAll);
-        Cover = root.findViewById(R.id.LoadingLayout);
-        Favon = root.findViewById(R.id.FavON);
-        Favoff = root.findViewById(R.id.FavOff);
-
-        New_Book_Pagination.populateData(API, ETC, queue, Wrap, items, Cover);
-        initAdapter();
-        New_Book_Pagination.initScrollListener(API, queue, Wrap, items, NewBookListAdapter, recyclerView, Store);
 
         try {
             FileReader fr = new FileReader(getActivity().getDataDir() + "/userInfo.json");
@@ -69,17 +55,37 @@ public class New_Tab_ALL extends Fragment {
             JSONObject jsonObject = new JSONObject(result);
             JSONObject UserInfo = jsonObject.getJSONObject("user");
             TOKEN = UserInfo.getString("token");
+            Log.d("TOKEN", TOKEN);
             Log.d("USERINFO", "읽기 완료");
         } catch (IOException | JSONException e) {
             e.printStackTrace();
             Log.d("USERINFO", "읽기 실패");
         }
 
+        if(!TOKEN.equals("")){
+            ETC = "&store=" + Store + "&orderby=redate&offset=25&page=" + 1 + "&token=" + TOKEN + "&class=";
+        } else {
+            ETC = "&store=" + Store + "&orderby=redate&offset=25&page=" + 1 + "&class=";
+        }
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        String API = "/v1/book/list.joa";
+
+        recyclerView = root.findViewById(R.id.Main_NewBookList);
+        Wrap = root.findViewById(R.id.Tab_NewAll);
+        Cover = root.findViewById(R.id.LoadingLayout);
+        Favon = root.findViewById(R.id.FavON);
+        Favoff = root.findViewById(R.id.FavOff);
+
+        New_Book_Pagination.populateData(API, ETC, queue, Wrap, items, Cover);
+        initAdapter();
+        New_Book_Pagination.initScrollListener(API, queue, Wrap, items, NewBookListAdapter, recyclerView, Store);
+        
         NewBookListAdapter.setOnItemClicklistener((holder, view, position, Value) -> {
             Main_BookListData item = NewBookListAdapter.getItem(position);
-            Log.d("Test--", item.getBookCode());
             New_Book_Pagination.FavToggle(queue, item.getBookCode(), TOKEN);
         });
+
 
         return root;
     }
@@ -88,7 +94,7 @@ public class New_Tab_ALL extends Fragment {
         NewBookListAdapter = new Main_BookListAdapter_New(items);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(NewBookListAdapter);
         NewBookListAdapter.notifyDataSetChanged();
+        recyclerView.setAdapter(NewBookListAdapter);
     }
 }
