13799ea5e52eb80d0557adef89258a29a4109a03
DawudKim <kdw0310@ajou.ac.kr>
2021-08-24 08:51:41 +0900

    2021.08.24 메인

--- app/src/main/java/bigbigdw/joaradw/book/BookListData.kt
+++ app/src/main/java/bigbigdw/joaradw/book/BookListData.kt
@@ -4,11 +4,7 @@ class BookListDataA(
     var writer: String,
     var title: String,
     var bookImg: String,
-    var isAdult: String,
-    var isFinish: String,
-    var isPremium: String,
-    var isNobless: String,
-    var intro: String,
+    var cntChapter: String,
     var bookCode: String,
 )
 
--- app/src/main/java/bigbigdw/joaradw/book/BookListResult.kt
+++ app/src/main/java/bigbigdw/joaradw/book/BookListResult.kt
@@ -1,4 +1,35 @@
 package bigbigdw.joaradw.book
 
-class BookListResult {
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+//작품 리스트
+class BookListAResult {
+    @SerializedName("books")
+    @Expose
+    val banner: List<BooksValue>? = null
+}
+
+//작품 리스트 상세
+class BooksValue {
+    @SerializedName("book_code")
+    @Expose
+    var bookCode: String? = null
+
+    @SerializedName("book_img")
+    @Expose
+    var bookImg: String? = null
+
+    @SerializedName("cnt_chapter")
+    @Expose
+    var cntChapter: String? = null
+
+    @SerializedName("subject")
+    @Expose
+    var subject: String? = null
+
+    @SerializedName("writer_name")
+    @Expose
+    var writerName: String? = null
 }
